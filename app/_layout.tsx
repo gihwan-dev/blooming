1b70bbbfdde98cd3d0d754b9d954f0ed2345efa5
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-10-03 13:03:48 +0900

    feat: 푸쉬알림 아이콘 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,13 +2,11 @@ import {Stack} from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import 'react-native-reanimated';
 
-import {useColorScheme} from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
 
   return (
       <Stack screenOptions={{
